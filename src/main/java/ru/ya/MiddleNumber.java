2ccd6cc673b006bc48105d552f608d3c59f68be8
Lectorem <79637710149@mail.ru>
2025-02-23 09:38:38 +0300

    fix for leetcode

--- src/main/java/ru/ya/MiddleNumber.java
+++ src/main/java/ru/ya/MiddleNumber.java
@@ -1,20 +1,22 @@
 package ru.ya;
 
-import java.util.Scanner;
-
 public class MiddleNumber {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+    public static int middleNumber(int[] numbers) {
+        int a = numbers[0];
+        int b = numbers[1];
+        int c = numbers[2];
 
         // Используем ручные проверки, чтобы избежать лишних вызовов Math.max и Math.min
         int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
         int min = (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
         int middle = a + b + c - max - min;
 
-        System.out.println(middle);
+        return middle;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+
+        System.out.println(middleNumber(numbers));
     }
 }
-

d0caff9c5a52028f8ca83a892852732e471e9e8f
Lectorem <79637710149@mail.ru>
2025-02-24 19:35:56 +0300

    add BusyStudent

---
+++ src/main/java/ru/ya/BusyStudent.java
@@ -0,0 +1,23 @@
+package ru.ya;
+
+public class BusyStudent {
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] startTime = { 1, 2, 3 };
+        int[] endTime = { 3, 2, 7 };
+        int queryTime = 4;
+
+        System.out.println(busyStudent(startTime, endTime, queryTime));
+    }
+}

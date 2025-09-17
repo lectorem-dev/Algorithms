de8714652554df9ffb7847dc1e767121cc539053
lectorem-dev <79637710149@mail.ru>
2025-09-17 09:11:59 +0300

    решена задача: MergeJsons2

--- src/main/java/ru/ya/warm/up/MergeJsons2.java
+++ src/main/java/ru/ya/warm/up/MergeJsons2.java
@@ -1,10 +1,8 @@
 package ru.ya.warm.up;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -14,30 +12,32 @@ import org.json.simple.parser.ParseException;
 public class MergeJsons2 {
     public static void main(String[] args) throws IOException, ParseException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String[] firstLine = reader.readLine().split(" ");
-        int n = Integer.parseInt(firstLine[0]);
-        int m = Integer.parseInt(firstLine[1]);
+        String[] firstLine = reader.readLine().trim().split("\\s+");
+        int n = Integer.parseInt(firstLine[0]); // количество фидов
+        int m = Integer.parseInt(firstLine[1]); // максимум офферов в итоговом фиде
 
         JSONParser parser = new JSONParser();
-        JSONArray allOffers = new JSONArray();
+        JSONArray resultOffers = new JSONArray();
 
-        for (int i = 0; i < n && allOffers.size() < m; i++) {
-            JSONObject feed = (JSONObject) parser.parse(reader.readLine());
+        for (int i = 0; i < n; i++) {
+            String line = reader.readLine();
+            JSONObject feed = (JSONObject) parser.parse(line);
             JSONArray offers = (JSONArray) feed.get("offers");
 
-            for (Object offerObj : offers) {
-                if (allOffers.size() >= m) break;
-                allOffers.add(offerObj);
+            for (Object offer : offers) {
+                if (resultOffers.size() < m) {
+                    //noinspection unchecked
+                    resultOffers.add(offer);
+                } else {
+                    break; // достигли лимита m
+                }
             }
         }
 
-        JSONObject result = new JSONObject();
-        result.put("offers", allOffers);
-        writer.write(result.toJSONString());
+        JSONObject resultFeed = new JSONObject();
+        //noinspection unchecked
+        resultFeed.put("offers", resultOffers);
 
-        reader.close();
-        writer.close();
+        System.out.println(resultFeed.toJSONString());
     }
 }
